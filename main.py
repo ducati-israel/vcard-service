52fee40e1d3c9e1791f6cb5380951cc42f901c14
jossef <jossef12@gmail.com>
2022-09-27 13:58:03 +0300

    feat: issuing vcard json to s3 bucket

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import hashlib
 import logging
 import os
+
+import boto3
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import json
@@ -11,9 +14,22 @@ dotenv.load_dotenv()
 
 GOOGLE_SPREADSHEET_ID = os.environ['GOOGLE_SPREADSHEET_ID']
 GOOGLE_SERVICE_ACCOUNT_CREDENTIALS = os.environ['GOOGLE_SERVICE_ACCOUNT_CREDENTIALS']
+
+AWS_S3_BUCKET_NAME = os.environ['AWS_S3_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_REGION = os.environ['AWS_REGION']
+
+aws_session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+s3_resource = aws_session.resource('s3')
+
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 HEADER_BOT_STATUS = 'סטטוס בוט'
+STATUS_ISSUE = 'הנפקה'
+STATUS_ISSUE_DONE = 'הנפקה - בוצע'
+STATUS_INPUT_ERROR = 'שגיאה'
+STATUS_INPUT_ERROR_DONE = 'שגיאה - בוצע'
 
 
 class GoogleSheetsClient:
@@ -104,11 +120,12 @@ def normalize_email_address(email_address):
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     with GoogleSheetsClient(GOOGLE_SERVICE_ACCOUNT_CREDENTIALS) as google_sheets_client:
-        print(google_sheets_client.headers)
         for item in google_sheets_client.items:
             membership_year = item['חברות']
             ducati_member_code = item['קוד דוקאטי']
+            role = item['תפקיד']
             hebrew_full_name = item['שם מלא בעברית']
             english_full_name = item['שם מלא באנגלית']
             email_address = item['כתובת אימייל']
@@ -117,20 +134,37 @@ def main():
             phone_number = normalize_phone_number(phone_number)
             motorcycle_model = item['דגם אופנוע נוכחי']
             registration_type = item['רישום יחיד או בזוג']
-            print(email_address, phone_number)
+            vcard_id = f'{email_address}:{phone_number}'
+            vcard_id = vcard_id.encode()
+            vcard_id = hashlib.sha1(vcard_id).hexdigest()
+            short_vcard_id = vcard_id[10:]
 
             bot_status = item.get(HEADER_BOT_STATUS, '')
-            if not bot_status:
-                continue
+            if bot_status == STATUS_ISSUE:
+                logging.info(f'issuing card for member id="{short_vcard_id}"')
 
-            if bot_status == 'להנפיק כרטיס':
-                print('issuing card')
-                google_sheets_client.set_item_field(item, HEADER_BOT_STATUS, 'הונפק כרטיס ונשלחה הודעה')
-                # TODO send sms message
+                vcard = {
+                    "hebrew_full_name": hebrew_full_name,
+                    "english_full_name": english_full_name,
+                    "membership_year": membership_year,
+                    "ducati_member_code": ducati_member_code,
+                    "role": role,
+                    "motorcycle_model": motorcycle_model,
+                    "registration_type": registration_type,
+                }
+                vcard = json.dumps(vcard)
+                vcard = vcard.encode('utf-8')
+                s3_resource.meta.client.put_object(Body=vcard, Bucket=AWS_S3_BUCKET_NAME, Key=f'{vcard_id}.json', ACL='public-read')
 
-            if bot_status == 'לשלוח הודעה שחסרים פרטים':
-                google_sheets_client.set_item_field(item, HEADER_BOT_STATUS, 'נשלחה הודעת שגיאה למשתמש')
+                google_sheets_client.set_item_field(item, HEADER_BOT_STATUS, STATUS_ISSUE_DONE)
                 # TODO send sms message
+                # TODO send email message
+
+            if bot_status == STATUS_INPUT_ERROR:
+                logging.info('notifying user of input error')
+                google_sheets_client.set_item_field(item, HEADER_BOT_STATUS, STATUS_INPUT_ERROR_DONE)
+                # TODO send sms message
+                # TODO send email message
 
 
 if __name__ == '__main__':
